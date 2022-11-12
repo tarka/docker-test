a4f5ab3761fdb1ed3d42454ab388a8c22b1ddd14
Steve Smith <tarkasteve@gmail.com>
2022-11-12 13:36:53 +1100

    Rename test

--- tests/lib.rs
+++ tests/lib.rs
@@ -2,7 +2,7 @@
 use docker_test as dt;
 
 #[test]
-fn help() {
+fn build_run() {
     let container = dt::setup("testproj", Some("testproj"), None, "1.64.0").unwrap();
     let out = container.exec(vec![container.dest_str(), "--help"]).unwrap();
     let stdout = String::from_utf8(out.stdout).unwrap();
